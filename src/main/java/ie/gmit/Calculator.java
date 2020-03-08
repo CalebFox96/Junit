083f8dd67ddfc4b46a9e4ae8d1856db2a2f72c1b
CalebFox96 <G00327455@gmit.ie>
2020-03-08 20:18:43 +0000

    Cacluator class updates

--- src/main/java/ie/gmit/Calculator.java
+++ src/main/java/ie/gmit/Calculator.java
@@ -21,4 +21,8 @@ public class Calculator {
         int result = num1 +num2;
         return result;
     }
+    public int delete(int num1,int num2){
+        int result = num1 -num2;
+        return result;
+    }
 }
--- src/test/java/ie/gmit/CalculatorTest.java
+++ src/test/java/ie/gmit/CalculatorTest.java
@@ -1,13 +1,21 @@
 package ie.gmit;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
     Calculator myCalc;
-@Test
-    void testAdd(){
+    @BeforeEach
+    void setup(){
         myCalc = new Calculator();
+    }
+    @Test
+    void testAdd(){
         assertEquals(10,myCalc.add(5,5));
+    }
+    @Test
+    void testDelete(){
+        assertEquals(5,myCalc.delete(10,5));
     }
 }
